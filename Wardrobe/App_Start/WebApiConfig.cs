d8cd66066fe73d25faf9ebaf9e6a842cf385b559
Jonny Pillar <09132921@stu.mumu.ac.uk>
2015-01-30 15:36:19 +0000

    Implemented a legendary API

--- Wardrobe/App_Start/WebApiConfig.cs
+++ Wardrobe/App_Start/WebApiConfig.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web.Http;
+﻿using System.Web.Http;
+using Newtonsoft.Json;
 
 namespace Wardrobe
 {
@@ -14,11 +12,11 @@ namespace Wardrobe
             // Web API routes
             config.MapHttpAttributeRoutes();
 
-            config.Routes.MapHttpRoute(
-                name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
-                defaults: new { id = RouteParameter.Optional }
-            );
+            config.Routes.MapHttpRoute("DefaultApi", "api/{controller}/{id}", new {id = RouteParameter.Optional}
+                );
+
+            config.Formatters.JsonFormatter.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+            config.Formatters.JsonFormatter.SerializerSettings.Formatting = Formatting.Indented;
         }
     }
 }
---
+++ Wardrobe/Controllers/DickController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+using System.Web.Http;
+
+namespace Wardrobe.Controllers
+{
+    public class DickController : ApiController
+    {
+        public IHttpActionResult Get()
+        {
+            var random = new Random();
+            return Ok(DickGenerator(random.Next(50)));
+        }
+
+        public IHttpActionResult Get(int id)
+        {
+            return Ok(DickGenerator(id));
+        }
+
+        private static string DickGenerator(int dickSize)
+        {
+            var dickBuilder = new StringBuilder();
+            dickBuilder.Append("3");
+            for (var i = 0; i < dickSize; i++)
+            {
+                dickBuilder.Append("=");
+            }
+            dickBuilder.Append("D");
+            return dickBuilder.ToString();
+        }
+    }
+}
